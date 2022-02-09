d5f7ac03077593f803ba7d5f5d6515a47643bc72
egumin <ethangumin@gmail.com>
2022-02-09 17:39:54 -0500

    remove junk code

--- src/scripts/graph_display.js
+++ src/scripts/graph_display.js
@@ -38,45 +38,7 @@ class GraphDisplay {
         return chartHeight - yScale(d.size);
       });
   }
-
-  // async quickSort(arr, start = 0, end = arr.length - 1) {
-  //   if (start >= end) return;
-
-  //   const idx = await this.quickSortHelperPartition(arr, start, end);
-
-  //   await Promise.all([
-  //     this.quickSort(arr, start, idx - 1),
-  //     this.quickSort(arr, idx + 1, end),
-  //   ]);
-  // }
-
-  // async quickSortHelperPartition(arr, start, end) {
-  //   let pivotIdx = start;
-  //   let pivotVal = arr[end].size;
-
-  //   debugger;
-
-  //   for (let i = start; i < end; i++) {
-  //     if (arr[i].size < pivotVal) {
-  //       [arr[i], arr[pivotIdx]] = [arr[pivotIdx], arr[i]];
-  //       pivotIdx++;
-  //     }
-  //   }
-
-  //   debugger;
-
-  //   d3.select("svg").remove();
-  //   this.createChart();
-
-  //   await this.sortDelay();
-
-  //   [arr[end], arr[pivotIdx]] = [arr[pivotIdx], arr[end]];
-
-  //   debugger;
-
-  //   return pivotIdx;
-  // }
-
+  
   sortDelay() {
     return new Promise((resolve) => {
       setTimeout(() => {
