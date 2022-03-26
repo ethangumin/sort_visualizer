d7c653f3dd185f3dcc8b9a1630c8ddb4ef9095fb
egumin <ethangumin@gmail.com>
2022-03-26 17:21:44 -0400

    selection sort bar colors

--- src/scripts/sorting_algorithms/selection_sort.js
+++ src/scripts/sorting_algorithms/selection_sort.js
@@ -5,22 +5,32 @@ async function selectionSort(data, delay) {
 
   for (let i = 0; i < len; i++) {
     let min = i;
-    // debugger;
     for (let k = i + 1; k < len; k++) {
+      d3.select("[data_id='" + min + "']")
+        .classed("sorting", true)
+        .classed("unsorted", false);
+      d3.select("[data_id='" + k + "']")
+        .classed("sorting", true)
+        .classed("unsorted", false);
       debugger;
       if (
         parseInt(d3.select("[data_id='" + k + "']").attr("height")) <
         parseInt(d3.select("[data_id='" + min + "']").attr("height"))
       ) {
+        d3.select("[data_id='" + min + "']")
+          .classed("sorting", false)
+          .classed("unsorted", true);
         min = k;
+        debugger;
+      } else {
+        d3.select("[data_id='" + k + "']")
+          .classed("sorting", false)
+          .classed("unsorted", true);
         debugger;
       }
     }
     if (min !== i) {
-      //   let temp = data[i];
-      //   data[i] = data[min];
-      //   data[min] = temp;
-      debugger;
+    //   debugger;
       let tempHeight = d3.select("[data_id='" + i + "']").attr("height");
       let tempY = d3.select("[data_id='" + i + "']").attr("y");
 
@@ -32,12 +42,32 @@ async function selectionSort(data, delay) {
         "y",
         d3.select("[data_id='" + min + "']").attr("y")
       );
+      d3.select("[data_id='" + i + "']")
+        .classed("sorted", true)
+        .classed("unsorted", false)
+        .classed("sorting", false);
 
       d3.select("[data_id='" + min + "']").attr("height", tempHeight);
       d3.select("[data_id='" + min + "']").attr("y", tempY);
+      d3.select("[data_id='" + min + "']")
+        .classed("sorted", false)
+        .classed("unsorted", true)
+        .classed("sorting", false);
       debugger;
     }
+
+    // temp
+    d3.select("[data_id='" + i + "']")
+      .classed("sorted", true)
+      .classed("unsorted", false)
+      .classed("sorting", false);
+      debugger;
   }
+
+  d3.selectAll("rect")
+    .classed("sorted", true)
+    .classed("unsorted", false)
+    .classed("sorting", false);
 
   removeDisableButton(document.getElementsByClassName("footer__reset")[0]);
 }
